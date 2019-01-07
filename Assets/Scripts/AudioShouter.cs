124dd2df413b861805c2ed813398e52d3169e663
januar elsan <januarelsan@gmail.com>
2019-01-07 14:33:56 +0700

    Initial commit

---
+++ Assets/Scripts/AudioShouter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioShouter : Singleton<AudioShouter> {
+
+	[SerializeField] private AudioClip[] clips;
+
+	public void ShoutClip(int clipIndex){
+		GetComponent<AudioSource>().PlayOneShot(clips[clipIndex]);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : Singleton<GameData> {
+
+	[SerializeField] private bool resetData;
+
+	void Awake(){
+		if(resetData)
+			PlayerPrefs.DeleteAll();
+	}
+
+	void Start(){
+		//Set First Level Opened
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 1; j++)
+			{
+				SetTypeLevelOpened(i,j,1);
+			}
+		}
+	}
+
+	public void SetLevelNum(int num){
+		PlayerPrefs.SetInt("LevelNum", num);
+	}
+
+	public int GetLevelNum(){
+		return PlayerPrefs.GetInt("LevelNum", 0);
+	}
+
+	public void SetGameType(int type){
+		PlayerPrefs.SetInt("GameType", type);
+	}
+
+	public int GetGameType(){
+		return PlayerPrefs.GetInt("GameType", 0);
+	}
+
+	public void SetTypeLevelOpened(int type, int level, int opened){
+		PlayerPrefs.SetInt("TypeLevel_" + type + "_" + level, opened);
+	}
+
+	public int GetTypeLevelOpened(int type, int level){
+		return PlayerPrefs.GetInt("TypeLevel_" + type + "_" + level, 0);
+	}
+}
